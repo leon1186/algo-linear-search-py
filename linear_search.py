7d508a3e6f51eab56a38a02af1a6cbaca48e8307
francisleon <francisleon1186@yahoo.com>
2025-11-14 16:31:34 -0500

    finishing the code

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,22 @@
-def linear_search(value_to_find, array_to_search_through):
-    # your code here
-    pass
+# linear_search.py
 
-def linear_search_global(value_to_find, array_to_search_through):
-    # your code here
-    pass
+# CamelCase version used in first test
+def linearSearch(target, list_to_search):
+    for index, value in enumerate(list_to_search):
+        if value == target:
+            return index
+    return None
+
+
+# snake_case version (tests also call this name)
+def linear_search(target, list_to_search):
+    return linearSearch(target, list_to_search)
+
+
+# return ALL matches (global search)
+def linear_search_global(target, list_to_search):
+    indices = []
+    for index, value in enumerate(list_to_search):
+        if value == target:
+            indices.append(index)
+    return indices
--- test_linear_search.py
+++ test_linear_search.py
@@ -1,17 +1,14 @@
-from linear_search import linear_search,linear_search_global
-import pytest
+from linear_search import linearSearch, linear_search, linear_search_global
 
 def test_linear_search_exists():
-    assert linear_search(3, [1,2,3]) == 2
+    assert linearSearch(3, [1,2,3]) == 2
 
-def test_linear_search_does_not_exist():
+def test_linearSearch_does_not_exist():
     assert linear_search(4, [1,2,3]) == None
 
 def test_linear_search_each_int_exists_but_not_whole():
     assert linear_search(13, [1,2,3]) == None
 
-
-# tests for linear_search_global
 def test_global_search_multiple_chars():
     assert linear_search_global("a", ["b", "a", "n", "a", "n", "a", "s"]) == [1, 3, 5]
 
